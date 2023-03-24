6082388cd97eaa29434a1bf1d5a458aa20244f4b
Ishaan-Wadhwa <ishaanwadhwa03@gmail.com>
2023-03-24 12:23:25 +0530

    added the firebase plugins  Changes to be committed: 	modified:   lib/Answer.dart 	modified:   lib/main.dart 	modified:   macos/Flutter/GeneratedPluginRegistrant.swift 	modified:   pubspec.lock 	modified:   pubspec.yaml

--- lib/Answer.dart
+++ lib/Answer.dart
@@ -1,24 +1,3 @@
-// import 'package:flutter/material.dart';
-// class Answer extends StatelessWidget
-// {
-//   final VoidCallback printText;
-//   Answer(this.printText);
-//   @override
-//   Widget build(BuildContext context)
-//   {
-//     return Container
-//     (
-//         width: double.infinity,
-//         child: ElevatedButton
-//         (
-//           // ignore: sort_child_properties_last
-//           // ignore: prefer_const_constructors
-//           child: Text('Answer 1', style: TextStyle(color: Colors.red)),
-//           onPressed: printText,
-//         )
-//         );
-//   }
-// }
 import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget 
@@ -35,9 +14,6 @@ class Answer extends StatelessWidget
       width: double.infinity,
       child: ElevatedButton(
         style:ElevatedButton.styleFrom(primary: Colors.blue),
-        // style: ButtonStyle(
-        //     backgroundColor: MaterialStateProperty.all(Colors.blue),
-        //     foregroundColor: MaterialStateProperty.all(Colors.black)),
         child: Text(answerText),
         onPressed: selectHandler(),
       ),
